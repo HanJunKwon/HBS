6ebf372e5dc7a7e495eee182a89c66d03704804c
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2026-03-18 16:14:33 +0900

    [긴급수정] 허니웰 스캐너에서 CR 또는 LF 뒤에 오는 Ctrl+J는 제어 문자를 처리하는 과정에서 남은 이벤트로 예외처리 추가함.

--- lib-hbs/src/main/java/com/hansae/hbs/BarcodeScanManager.kt
+++ lib-hbs/src/main/java/com/hansae/hbs/BarcodeScanManager.kt
@@ -17,11 +17,6 @@ object BarcodeScanManager {
     }
 
     fun eventKeyToBarcode(event: KeyEvent) {
-        // 허니웰 바코드 스캐너에서 Enter가 들어온 후에 "J" 데이터가 입력되어 다음 데이터를 읽어오는데 문제가 있음.
-        if (barcodeBuffer.toString() == "J") {
-            barcodeBuffer.clear()
-        }
-
         if (event.action != KeyEvent.ACTION_DOWN) return
 
         val keyCode = event.keyCode
@@ -45,6 +40,10 @@ object BarcodeScanManager {
     private fun mapKeyCodeToChar(event: KeyEvent): Char? {
         val keyCode = event.keyCode
         val isShift = event.isShiftPressed
+
+        if(keyCode == KeyEvent.KEYCODE_J && event.isCtrlPressed) {
+            return null
+        }
 
         return when (keyCode) {
             // 숫자 및 숫자 키의 특수문자 (!@#$%^&*() )
